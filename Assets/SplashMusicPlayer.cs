d2a4c4cb4061fc9af383c90a6193d028906868e2
lshi <lshi@bu.edu>
2020-06-29 17:26:25 -0400

    import utility, added circuit for camera moving (camera not moving yet)

--- Assets/SplashMusicPlayer.cs
+++ Assets/SplashMusicPlayer.cs
@@ -6,6 +6,11 @@ using UnityEngine.SceneManagement;
 public class SplashMusicPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
+    private void Awake() 
+    {
+        DontDestroyOnLoad(this.gameObject);        
+    }
+    
     void Start()
     {
         Invoke("LoadFirstScene",5f);
