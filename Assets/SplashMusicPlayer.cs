a19d311f39b40a363ee48eed065c78dd9e13b11c
lshi <lshi@bu.edu>
2020-06-29 15:08:17 -0400

    add a splash scene with looped music and load scene function

---
+++ Assets/SplashMusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashMusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstScene",5f);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
