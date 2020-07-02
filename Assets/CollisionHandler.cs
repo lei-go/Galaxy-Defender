214bc113c16c1f2a564bab98fc5de8854e7ea40f
lshi <lshi@bu.edu>
2020-07-02 14:07:08 -0400

    added a collision handler

---
+++ Assets/CollisionHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    [Tooltip("In Seconds")][SerializeField] float reloadDelay = 1f;
+    [Tooltip("FX-prefabs On Player")][SerializeField] GameObject deathFX;
+    
+        private void OnTriggerEnter(Collider other) 
+    {
+        print("triggered");     
+        SendMessage("OnPlayerDeath");   
+        deathFX.SetActive(true);
+        Invoke("ReloadScene",reloadDelay);   
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
